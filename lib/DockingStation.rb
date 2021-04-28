5b82a2e8bd285021720cc23ee8e3ac59a86718dd
Elliott <ells101@hotmail.co.uk>
2021-04-28 14:16:03 +0100

    addded comments

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,6 +1,6 @@
-require_relative 'bike'
-class DockingStation
-  def release_bike
+require_relative 'bike' # require doesn't work here, need require relative
+class DockingStation #  used when you referring to a file thats relative to 
+  def release_bike # the current file you are working on (basically, within the same project directory).
     Bike.new
   end
 end
