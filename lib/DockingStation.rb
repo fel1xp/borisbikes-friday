1c4c36c5fcd66d8eb4b7750fb862c2645e3891ff
Elliott <ells101@hotmail.co.uk>
2021-04-29 16:25:46 +0100

    chapter 11 complete

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,8 +1,11 @@
 require_relative 'bike' # require doesn't work here, need require relative
 class DockingStation #  used when you referring to a file thats relative to 
-  attr_reader :bike
+  attr_reader :bike  
   def release_bike # the current file you are working on (basically, within the same project directory).
+    raise "ERROR! You can't do that... Docking Station is empty." if bike.nil?
+    
     Bike.new
+
   end
   def dock(bike)
     @bike = bike
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,13 +1,29 @@
 require 'DockingStation'
 describe DockingStation do
-  describe 'responds to #release_bike' do
-    subject { DockingStation.new }
+  describe 'responds to #release_bike' do  
+  subject { DockingStation.new }
     it { is_expected.to respond_to(:release_bike) }
   end
-  describe '#release_bike returns a Bike that responds to #working?' do
-    subject { DockingStation.new.release_bike }
+  describe 'if a bike has been docked, then release a bike ' do
+    #arrange
+    station = DockingStation.new
+    bike = Bike.new
+    #act
+    station.dock(bike)
+    #assert
+    subject { station.release_bike }
     it { is_expected.to respond_to(:working?) }
   end
+
+  describe '#release_bike' do
+    #Arrange
+    station = DockingStation.new
+    #Assert
+    it 'raises an error when station is empty' do 
+      expect {station.release_bike}.to raise_error 
+    end
+  end 
+
 
   describe 'responds to #dock' do
     subject { DockingStation.new }
