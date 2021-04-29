807ea55dabe8ec57fc6851d104f6f8c032b64308
Elliott <ells101@hotmail.co.uk>
2021-04-29 15:20:38 +0100

    chapter 11 complete

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,6 +1,10 @@
 require_relative 'bike' # require doesn't work here, need require relative
 class DockingStation #  used when you referring to a file thats relative to 
+  attr_reader :bike
   def release_bike # the current file you are working on (basically, within the same project directory).
     Bike.new
+  end
+  def dock(bike)
+    @bike = bike
   end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -8,4 +8,23 @@ describe DockingStation do
     subject { DockingStation.new.release_bike }
     it { is_expected.to respond_to(:working?) }
   end
+
+  describe 'responds to #dock' do
+    subject { DockingStation.new }
+    it { is_expected.to respond_to(:dock)}
+  end
+
+  describe '#bike' do
+    it 'responds with a bike that has been docked' do
+      # Arrange
+      station = DockingStation.new
+      bike = Bike.new
+      
+      # Act
+      station.dock(bike)
+
+      # Assert
+      expect(station.bike).to eq(bike)
+    end
+  end
 end
