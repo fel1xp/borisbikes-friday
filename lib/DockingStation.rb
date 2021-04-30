35351afce68b94977b9981db0475dffda21f0cb0
Felix <felixcpiercy@gmail.com>
2021-04-30 16:23:56 +0100

    challenge 14

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,14 +1,19 @@
 require_relative 'bike' # require doesn't work here, need require relative
-class DockingStation #  used when you referring to a file thats relative to 
-  attr_reader :bike  
-  def release_bike # the current file you are working on (basically, within the same project directory).
-    raise "ERROR! You can't do that... Docking Station is empty." if bike.nil?
-    
-    Bike.new
 
+class DockingStation #  used when you referring to a file thats relative to 
+  attr_reader :bikes  
+
+  def initialize
+    @bikes = []
   end
+
+  def release_bike # the current file you are working on (basically, within the same project directory). 
+    raise "ERROR! You can't do that... Docking Station is empty." if @bikes.count == 0
+    @bikes.pop
+  end
+
   def dock(new_bike)
-    raise "ERROR! Docking station is full." if bike
-    @bike = new_bike
+    raise "ERROR! Docking station is full." if @bikes.count >= 20 
+    @bikes << new_bike
   end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,46 +1,17 @@
 require 'DockingStation'
 describe DockingStation do
-  describe 'responds to #release_bike' do  
-  subject { DockingStation.new }
-    it { is_expected.to respond_to(:release_bike) }
-  end
-  describe 'if a bike has been docked, then release a bike ' do
-    #arrange
-    station = DockingStation.new
-    bike = Bike.new
-    #act
-    station.dock(bike)
-    #assert
-    subject { station.release_bike }
-    it { is_expected.to respond_to(:working?) }
-  end
+  
   
   describe '#dock' do
-    #Arrange
-    station = DockingStation.new
-    bike = Bike.new    
-    #act
-    station.dock(bike)
-    #assert
     it 'raises an error when station is full' do
+      #Arrange
+      station = DockingStation.new
+      bike = Bike.new    
+      #act
+      20.times { station.dock(bike) }
+      #assert
       expect { station.dock(bike) }.to raise_error
     end
-  end 
-  describe '#release_bike' do
-    #Arrange
-    station = DockingStation.new
-    #Assert
-    it 'raises an error when station is empty' do 
-      expect { station.release_bike }.to raise_error 
-    end
-  end 
-
-  describe 'responds to #dock' do
-    subject { DockingStation.new }
-    it { is_expected.to respond_to(:dock)}
-  end
-
-  describe '#bike' do
     it 'responds with a bike that has been docked' do
       # Arrange
       station = DockingStation.new
@@ -50,7 +21,24 @@ describe DockingStation do
       station.dock(bike)
 
       # Assert
-      expect(station.bike).to eq(bike)
+      expect(station.bikes).to eq([bike])
     end
-  end
+  end 
+  describe '#release_bike' do
+    it 'raises an error when station is empty' do
+      #Arrange
+      station = DockingStation.new
+      #Assert
+     
+      expect { station.release_bike }.to raise_error 
+    end
+    it 'if a bike has been docked, then release a bike ' do
+      #arrange
+      bike = Bike.new
+      #act
+      subject.dock(bike)
+      #assert
+      expect(subject.release_bike).to eq bike
+    end
+  end 
 end
