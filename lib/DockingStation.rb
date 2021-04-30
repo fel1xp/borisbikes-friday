a580fe1bff49a47a3379785fbc3068bbabb5aeb0
Felix <felixcpiercy@gmail.com>
2021-04-30 16:28:30 +0100

    challenge 15

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -8,12 +8,21 @@ class DockingStation #  used when you referring to a file thats relative to
   end
 
   def release_bike # the current file you are working on (basically, within the same project directory). 
-    raise "ERROR! You can't do that... Docking Station is empty." if @bikes.count == 0
+    raise "ERROR! You can't do that... Docking Station is empty." if empty?
     @bikes.pop
   end
 
   def dock(new_bike)
-    raise "ERROR! Docking station is full." if @bikes.count >= 20 
+    raise "ERROR! Docking station is full." if full?
     @bikes << new_bike
+  end
+
+  private
+  def full?
+    @bikes.count >= 20 
+  end
+
+  def empty?
+    @bikes.count == 0
   end
 end
